1b98b05626566c7c79fa3d323273ff9aff46a7fe
algerd <algerd.efimov@mail.ru>
2020-09-10 22:42:42 +0300

    create project

---
+++ Tracer.Tests/TracerTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tracer.Tests
+{
+    [TestClass]
+    public class TracerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
